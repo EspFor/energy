9de993b245df44d54b24cc320856fe31d2b334b7
Espen Forså <espen.forsaa@pwc.com>
2019-10-31 10:36:26 +0100

    Test

---
+++ R/functions.R
@@ -0,0 +1,24 @@
+get_energy_data <- function(){
+
+
+
+
+}
+
+get_production <- function(.data, powerplant = NA){
+
+
+aqwvgfws
+
+ef
+
+}
+
+get_production2 <- function(.data, powerplant = NA){
+
+
+  aqwvgfws
+
+  ef
+
+}
